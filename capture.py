11d5dbb5cc66c82860019d092094f6e130e83749
adelaidor2Public <adelaido_r2@hotmail.com>
2015-12-14 19:22:10 -0600

    Update capture.py

--- capture.py
+++ capture.py
@@ -1,39 +1,57 @@
-#source ~/.profile
-#workon cv
-
+#!~/.virtualenvs/cv3/bin/python
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from time import sleep
 import numpy as np
 import cv2
-#import cv2.cv as cv
+import os
+from time import sleep
+import RPi.GPIO as GPIO
+
+import subprocess
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(04, GPIO.IN)
+GPIO.setup(14, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
 
 camera = PiCamera()
 camera.resolution = (1280, 720)
-
+    
 cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-img = cv2.imread("graycode_h1.bmp",0)
+img = cv2.imread("/home/pi/Documents/Rasp/solid_black.png",0)
 cv2.imshow("window",img)
-cv2.waitKey(250)
-#var1 = "graycode_h1.bmp"
-#img = cv2.imread(var1,0)
-#cv2.imshow("window",img)
-#var1 = "graycode_h2.bmp"
-#img = cv2.imread(var1,0)
-#cv2.imshow("window",img)
-#cv2.waitKey(6)
+cv2.waitKey(200)
+cv2.imshow("window",img)
+cv2.waitKey(600)
+
+#Ensure busy LED is off and ensure ready LED is on
+GPIO.output(14, 0);
+GPIO.output(15, 1);
 
 captures = [[], []]
 
+while(True):
+    if GPIO.input(04) == False:
+        break
+
+#turn on busy LED on and turn off ready LED
+GPIO.output(14, 1);
+GPIO.output(15, 0);
+
+hi = 0
 for x in range (1,3):
     if x == 1:
         var0 = "h"
+        hi = 8
     else:
         var0 = "v"
+        hi = 9
 
-    for y in range (1,9):
-        var1 = "graycode_" + var0 + str(y) + ".bmp"
+    for y in range (1,hi):
+        var1 = "/home/pi/Documents/Rasp/graycode_" + var0 + str(y) + ".bmp"
         img = cv2.imread(var1)
         cv2.imshow("window",img)
         cv2.waitKey(1)
@@ -43,22 +61,22 @@ for x in range (1,3):
         camera.capture(rawCapture, format="bgr")
         captures[x-1].append(rawCapture.array)
 
-
+img = cv2.imread("/home/pi/Documents/Rasp/solid_black.png",0)
+cv2.imshow("window",img)
+cv2.waitKey(100)
 cv2.destroyWindow("window")       
 cv2.destroyAllWindows()
 cv2.waitKey(1000)
+
 #write images to memory
 i = 0
 j = 0
 for orientation in captures:
     for capture in orientation:
-        name = "img_" + str(i) + "_" + str(j) + ".png"
+        name = "/home/pi/Documents/Rasp/img_" + str(i) + "_" + str(j) + ".png"
         cv2.imwrite(name, capture)
         j += 1
     j = 0
     i += 1
 
-    
-#camera.start_preview()
-#sleep(20)
-#camera.stop_preview()
+subprocess.call("./analyze.out 7 8 1280 720", shell = True)
