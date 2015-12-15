4447b9c88f317a9bcefbd4836e135d99deab1730
adelaidor2Public <adelaido_r2@hotmail.com>
2015-12-14 19:24:36 -0600

    Update debugger_correspondence.cpp

--- debugger_correspondence.cpp
+++ debugger_correspondence.cpp
@@ -13,12 +13,22 @@ INCLUDES
 using namespace std;
 using namespace cv;
 
-int main(int argc, char** argv){
+Vec3b getColor(int y, int x) {
+	float xpercent = (float) x/1280;
+	float ypercent = (float) y/720;
+	int r = 255.0*xpercent;
+	int g = 255.0 - 255.0*xpercent;
+	int b = 255.0*ypercent;
+	return Vec3b(b,g,r);
+}
+
+int main(int argc, char** argv) {
 
 	int imageWidth = 1280;
 	int imageHeight = 720;
 	fstream fs("correspondence.txt", std::fstream::in | std::fstream::out);
-	Mat testImage(imageHeight, imageWidth, CV_8UC3, Scalar(0,0,0));
+	Mat testImage1(imageHeight, imageWidth, CV_8UC3, Scalar(0,0,0));
+	Mat testImage2(imageHeight, imageWidth, CV_8UC3, Scalar(0,0,0));
 
 	bool isValid;
 	int x;
@@ -29,16 +39,37 @@ int main(int argc, char** argv){
 			fs >> x;
 			fs >> y;
 			if (isValid) {
-				testImage.at<Vec3b>(y, x) = Vec3b(255, 255, 255);
+				testImage1.at<Vec3b>(y, x) = getColor(row, col);
+				testImage2.at<Vec3b>(row, col) = getColor(row, col);
 			}
 		}
 	}
 	fs.close();
 	
-	imwrite("correspondence.png", testImage);
-	namedWindow("Display window", WINDOW_AUTOSIZE );
-   	imshow("Display window", testImage );
+	imwrite("correspondence1.png", testImage1);
+	namedWindow("projection window", WINDOW_AUTOSIZE );
+   	imshow("projection window", testImage1 );
     	waitKey(0); 
+	
+	/*
+	fs.open("correspondence.txt", std::fstream::in | std::fstream::out);
+	for (unsigned int row = 0; row < imageHeight; row++) {
+		for (unsigned int col = 0; col < imageWidth; col++) {
+			fs >> isValid;
+			fs >> x;
+			fs >> y;
+			if (isValid) {
+				testImage2.at<Vec3b>(row, col) = Vec3b(255, 255, 255);
+			}
+		}
+	}
+	fs.close();
+	*/
+
+	imwrite("correspondence2.png", testImage2);
+	namedWindow("camera window", WINDOW_AUTOSIZE );
+   	imshow("camera window", testImage2 );
+	waitKey(0);
 	
 	return 0;
 }
