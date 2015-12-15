0544f8d213129e62433a3313a7786c7a4b3e6449
adelaidor2Public <adelaido_r2@hotmail.com>
2015-12-14 19:25:23 -0600

    Update procamcalib.py

--- procamcalib.py
+++ procamcalib.py
@@ -11,66 +11,37 @@ import cv2
 camera = PiCamera()
 camera.resolution = (1280, 720)
 
+captures = []
+
 cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-img = cv2.imread("solid_black.png",0)
+img = cv2.imread("chessboard_1280_1024.png",0)
 cv2.imshow("window",img)
 cv2.waitKey(100)
-#var1 = "graycode_h1.bmp"
-#img = cv2.imread(var1,0)
-#cv2.imshow("window",img)
-#var1 = "graycode_h2.bmp"
-#img = cv2.imread(var1,0)
-#cv2.imshow("window",img)
-#cv2.waitKey(6)
 
-captures = [[], []]
+var1 = "newchessboard.png"
+img2 = cv2.imread(var1)
 
-var1 = "boardwithblack.jpg"
-img = cv2.imread(var1)
-cv2.imshow("window",img)
-cv2.waitKey(500)
-rawCapture = PiRGBArray(camera)
-camera.hflip = True
-camera.vflip = True
-camera.capture(rawCapture, format="bgr")
-x=1
-captures[x-1].append(rawCapture.array)
-
-
-
-#for x in range (1,3):
-#   if x == 1:
-#        var0 = "h"
-#    else:
-#        var0 = "v"
-#
-#    for y in range (1,7):
-#        var1 = "graycode_" + var0 + str(y) + ".bmp"
-#        img = cv2.imread(var1,0)
-#        cv2.imshow("window",img)
-#        cv2.waitKey(1)
-#        rawCapture = PiRGBArray(camera)
-#        #camera.hflip = True
-#        camera.vflip = True
-#        camera.capture(rawCapture, format="bgr")
-#        captures[x-1].append(rawCapture.array)
-
+for x in range (1,21):
+    cv2.imshow("window",img2)
+    cv2.waitKey(0)
+    rawCapture = PiRGBArray(camera)
+    camera.hflip = True
+    camera.vflip = True
+    camera.capture(rawCapture, format="bgr")
+    #name = "procam" + str(x) + ".jpg"
+    captures.append(rawCapture.array)
+    #cv2.imwrite(name, rawCapture.array)
+    #captures.append(rawCapture.array)
+    cv2.imshow("window",img)
+    cv2.waitKey(100)
 
 cv2.destroyWindow("window")       
 cv2.destroyAllWindows()
-#cv2.waitKey(1000)
-#write images to memory
-i = 0
-j = 0
-for orientation in captures:
-    for capture in orientation:
-        name = "procam.jpg"
-        cv2.imwrite(name, capture)
-        j += 1
-    i += 1
 
-    
-#camera.start_preview()
-#sleep(20)
-#camera.stop_preview()
+y = 1
+
+for num in captures:
+    name = "procam" + str(y) + ".jpg"
+    cv2.imwrite(name, num)
+    y += 1
